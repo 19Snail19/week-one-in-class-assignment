f6191ab403518fa55e21dc0226f101bb135454a0
James McKinney <jamesmckinney19@gmail.com>
2025-10-29 22:10:26 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("If you have never stolen a sweetroll, press any key.");
+Console.ReadKey(true);
+Console.WriteLine("\nLiar.");
+Console.WriteLine("\nPress any key to run from your shame.");
+Console.ReadKey(true);
